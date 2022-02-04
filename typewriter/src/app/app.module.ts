8e07e8986a9597c8832c251150b8660deae24946
Joshua Park <joshuapark823@gmail.com>
2022-02-03 20:09:44 -0500

    landing

--- typewriter/src/app/app.module.ts
+++ typewriter/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaperComponent } from './components/paper/paper.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaperComponent
+    PaperComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
